196b1643febb103ae344bc14bce8d0d0799d0856
Eleazar <eleazarfhernandez@gmail.com>
2023-09-26 11:00:14 -0500

    refactored Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
 import './App.css';
-import React, {useState} from "react";
+import Header from "./components/Header.jsx";
 
-const Header = () => {
-    return(
-        <div className={'headerBar'}>
-            <h1>Hello User!</h1>
-        </div>
+
+
+const App = () => {
+    return (
+        <Header />
     )
-}
-
-//working on sending the data to the console *done*
-//next is to create an API endpoint to send the information to a json file on server.js
-const InputField = () => {
-const [inputText, setInputText] = useState("");
-
-const inputHandle = (event) => {
-    event.preventDefault();
-
-    setInputText(`${inputText}`);
-    setInputText('')
-}
-        return (
-            <form onSubmit={inputHandle}>
-                <input className={'address-input-field'} name={'inputMain'} value={inputText} onChange={(event) => setInputText(event.target.value)} placeholder={"Write here.."} />
-                <button className={'address-input-button'}>Clear</button>
-                <p>You typed: {inputText}</p>
-            </form>
-        )
-}
-const LookUpPage = () => {
-  return (
-      <div className={'center-piece-lookup'}>
-        <p>See what you typed!</p>
-          <p>Enter your word or phrase below</p>
-          <InputField />
-      </div>
-  )
-}
-
-class App extends React.Component {
-
-    render () {
-        return (
-            <>
-                <Header />
-                <LookUpPage />
-            </>
-
-        )
-    }
 }
 
 export default App;
