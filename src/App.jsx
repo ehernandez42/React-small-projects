14b07961fa0649905ed5b5933853b6528e27aa03
Eleazar <eleazarfhernandez@gmail.com>
2023-09-27 11:37:05 -0500

    Created Faq section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Carousel from "./components/Carousel.jsx";
+import Faq from "./components/faq.jsx";
 
 const App = () => {
     return (
+        <div>
         <Carousel />
+        <Faq />
+        </div>
     )
 }
 
